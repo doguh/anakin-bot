fae65a0db6e8790598ef678ca83c070717211967
Hugo Denisse <hugodenisse@madboxgames.io>
2020-03-19 16:25:13 +0100

    add "i dont like sand" quote

--- index.js
+++ index.js
@@ -17,15 +17,34 @@ bot.on("message", (user, userID, channelID, message, evt) => {
   if (userID === bot.id) {
     return;
   }
+  iDontLikeSand(user, channelID, message);
+  notJustTheMen(user, channelID, message);
+});
+
+function notJustTheMen(user, channelID, message) {
   const found = message.match(/[A-Za-zÀ-ÿ0-9_]*men[A-Za-zÀ-ÿ0-9_]*/i);
   if (found) {
-    logger.debug(`Triggered by ${user}'s message: ${message}`);
+    logger.debug(`Not just the men triggered by ${user}'s message: ${message}`);
     const men = found[0];
     const women = men.replace(/men/i, "women");
     const children = men.replace(/men/i, "children");
     bot.sendMessage({
       to: channelID,
-      message: `Not just the ${men}, but the ${women} and ${children} too`
+      message: `Not just the ${men}, but the ${women} and ${children} too.`
     });
+    return true;
   }
-});
+}
+
+function iDontLikeSand(user, channelID, message) {
+  const found = message.match(/[A-Za-zÀ-ÿ0-9_]*sand[A-Za-zÀ-ÿ0-9_]*/i);
+  if (found) {
+    logger.debug(`I dont like sand triggered by ${user}'s message: ${message}`);
+    const sand = found[0];
+    bot.sendMessage({
+      to: channelID,
+      message: `I don't like ${sand}. It's coarse and rough and irritating and it gets everywhere.`
+    });
+    return true;
+  }
+}
